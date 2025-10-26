b3816826eff82c4550dcb7c023a51be48992860f
vitthal4545 <vitthalgs1434@gmail.com>
2025-10-26 23:19:09 +0530

    added editor.pages.jsx component

---
+++ src/pages/editor.pages.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState } from "react";
+import { userContext } from "../App.jsx";
+import { Navigate } from "react-router-dom";
+import BlogEditor from "../components/blog-editor.component.jsx";
+import PublishForm from "../components/publish-form.component.jsx";
+
+const blogStructure = {
+  title: "",
+  banner: "",
+  content: [],
+  tags: [],
+  des: "",
+  author: { personal_info: { fullName: "" } },
+};
+
+export const EditorContext = createContext({});
+
+function Editor() {
+  const [blog, setBlog] = useState(blogStructure);
+  const [editorState, setEditorState] = useState("editor");
+  const [textEditor, setTextEditor] = useState({ isReady: false });
+
+  let {
+    userAuth: { access_token },
+  } = useContext(userContext);
+
+  return (
+    <EditorContext.Provider
+      value={{
+        blog,
+        setBlog,
+        editorState,
+        setEditorState,
+        textEditor,
+        setTextEditor,
+      }}
+    >
+      {access_token === null ? (
+        <Navigate to="/signin" />
+      ) : editorState === "editor" ? (
+        <BlogEditor />
+      ) : (
+        <PublishForm />
+      )}
+    </EditorContext.Provider>
+  );
+}
+
+export default Editor;
