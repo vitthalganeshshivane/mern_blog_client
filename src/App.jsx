3a871bfb890eb3d237bb2e283978f6be4701fc9f
vitthal4545 <vitthalgs1434@gmail.com>
2025-10-26 23:25:49 +0530

    update app.jsx file for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { Route, Routes } from "react-router-dom";
+import { json, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { Toaster } from "react-hot-toast";
+import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
+import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
+
+export const userContext = createContext({});
 
 const App = () => {
+  const [userAuth, setUserAuth] = useState({ access_token: null });
+
+  useEffect(() => {
+    let userInSession = lookInSession("user");
+
+    userInSession
+      ? setUserAuth(JSON.parse(userInSession))
+      : setUserAuth({ access_token: null });
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
-      </Route>
-    </Routes>
+    <>
+      <userContext.Provider value={{ userAuth, setUserAuth }}>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<HomePage />} />
+            <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+            <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+          </Route>
+        </Routes>
+      </userContext.Provider>
+    </>
   );
 };
 
