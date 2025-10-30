8822e709210e53b77bfe6bdcefd45a614be825e5
vitthal4545 <vitthalgs1434@gmail.com>
2025-10-30 23:08:33 +0530

    Add dynamic segment to search route for query input

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
 import HomePage from "./pages/home.page";
+import SearchPage from "./pages/search.page";
 
 export const userContext = createContext({});
 
@@ -30,6 +31,7 @@ const App = () => {
             <Route index element={<HomePage />} />
             <Route path="signin" element={<UserAuthForm type="sign-in" />} />
             <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+            <Route path="search/:query" element={<SearchPage />} />
           </Route>
         </Routes>
       </userContext.Provider>
