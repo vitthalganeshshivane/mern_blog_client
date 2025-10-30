be96b363be32b2f028ba34bd53dfe6a06955669f
vitthal4545 <vitthalgs1434@gmail.com>
2025-10-30 23:10:24 +0530

    Implement search navigation on enter key press in Navbar

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet, useNavigate } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import { userContext } from "../App";
 import UserNavigationPanel from "./user-navigation.component";
@@ -7,6 +7,8 @@ import UserNavigationPanel from "./user-navigation.component";
 function Navbar() {
   const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
   const [userNavPanel, setUserNavPanel] = useState(false);
+
+  let navigate = useNavigate();
 
   const {
     userAuth,
@@ -21,6 +23,14 @@ function Navbar() {
     setTimeout(() => {
       setUserNavPanel(false);
     }, 500);
+  };
+
+  const handleSearch = (e) => {
+    let query = e.target.value;
+
+    if (e.keyCode === 13 && query.length) {
+      navigate(`/search/${query}`);
+    }
   };
 
   return (
@@ -39,6 +49,7 @@ function Navbar() {
             type="text"
             placeholder="search"
             className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+            onKeyDown={handleSearch}
           />
           <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
         </div>
