3f005a848f350c565214a2a5c724e6343fc18476
vitthal4545 <vitthalgs1434@gmail.com>
2025-10-31 21:22:01 +0530

    add user profile route with dynamic ID

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
 import HomePage from "./pages/home.page";
 import SearchPage from "./pages/search.page";
+import PageNotFound from "./pages/404.page";
+import ProfilePage from "./pages/profile.page";
 
 export const userContext = createContext({});
 
@@ -32,6 +34,7 @@ const App = () => {
             <Route path="signin" element={<UserAuthForm type="sign-in" />} />
             <Route path="signup" element={<UserAuthForm type="sign-up" />} />
             <Route path="search/:query" element={<SearchPage />} />
+            <Route path="user/:id" element={<ProfilePage />} />
           </Route>
         </Routes>
       </userContext.Provider>
