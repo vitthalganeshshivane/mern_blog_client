545d6ca1504150311d411500e8fdeb645222545c
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-01 21:27:14 +0530

    add utility function to format full date with day, month, and year

--- src/common/date.jsx
+++ src/common/date.jsx
@@ -27,3 +27,8 @@ export const getDay = (timestamp) => {
   let date = new Date(timestamp);
   return `${date.getDate()} ${months[date.getMonth()]}`;
 };
+
+export const getFullDay = (timestamp) => {
+  let date = new Date(timestamp);
+  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+};
