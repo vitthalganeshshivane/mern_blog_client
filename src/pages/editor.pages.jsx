fa46c3934dbef1fa3e0552b030a0c7cee7c012ee
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-06 22:54:11 +0530

    add blog loading and editing support with blog_id param and loading state

--- src/pages/editor.pages.jsx
+++ src/pages/editor.pages.jsx
@@ -1,8 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { userContext } from "../App.jsx";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import BlogEditor from "../components/blog-editor.component.jsx";
 import PublishForm from "../components/publish-form.component.jsx";
+import Loader from "../components/loader.component";
+import axios from "axios";
 
 const blogStructure = {
   title: "",
@@ -16,13 +18,37 @@ const blogStructure = {
 export const EditorContext = createContext({});
 
 function Editor() {
+  let { blog_id } = useParams();
+
   const [blog, setBlog] = useState(blogStructure);
   const [editorState, setEditorState] = useState("editor");
   const [textEditor, setTextEditor] = useState({ isReady: false });
+  const [loading, setLoading] = useState(true);
 
   let {
     userAuth: { access_token },
   } = useContext(userContext);
+
+  useEffect(() => {
+    if (!blog_id) {
+      return setLoading(false);
+    }
+
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", {
+        blog_id,
+        draft: true,
+        mode: "edit",
+      })
+      .then(({ data: { blog } }) => {
+        setBlog(blog);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setBlog(null);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <EditorContext.Provider
@@ -37,6 +63,8 @@ function Editor() {
     >
       {access_token === null ? (
         <Navigate to="/signin" />
+      ) : loading ? (
+        <Loader />
       ) : editorState === "editor" ? (
         <BlogEditor />
       ) : (
