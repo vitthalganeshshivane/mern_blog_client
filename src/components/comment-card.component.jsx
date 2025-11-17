992be0e309027e54a45af77d7f4f929e72b7d851
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-17 22:57:52 +0530

    add CommentCard component to display blog comments with user info

---
+++ src/components/comment-card.component.jsx
@@ -0,0 +1,30 @@
+import { getDay } from "../common/date";
+
+const CommentCard = ({ index, leftVal, commentData }) => {
+  let {
+    commented_by: {
+      personal_info: { profile_img, fullname, username },
+    },
+    commentedAt,
+    comment,
+  } = commentData;
+
+  return (
+    <div className="w-full" style={{ paddingLeft: `${leftVal * 10}px` }}>
+      <div className="my-5 p-6 rounded-md border border-9">
+        <div className="flex gap-3 items-center mb-8">
+          <img src={profile_img} className="w-6 h-6 rounded-full" alt="" />
+          <p className="line-clamp-1">
+            {fullname} @{username}
+          </p>
+          <p className="min-w-fit">{getDay(commentedAt)}</p>
+        </div>
+        <p className="font-gelasio text-xl ml-3">{comment}</p>
+
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentCard;
