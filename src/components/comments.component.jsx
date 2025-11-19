24cb1214146e9c75fdecd97626e7d82a87c83a6d
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-19 22:06:41 +0530

    add CommentsContainer component with fetchComments utility and comment display

---
+++ src/components/comments.component.jsx
@@ -0,0 +1,102 @@
+import { useContext } from "react";
+import { BlogContext } from "../pages/blog.page";
+import CommentField from "./comment-field.component";
+import axios from "axios";
+import { resolveValue } from "react-hot-toast";
+import NoDataMessage from "./nodata.component";
+import AnimationWrapper from "../common/page-animation";
+import CommentCard from "./comment-card.component";
+
+export const fetchComments = async ({
+  skip = 0,
+  blog_id,
+  setParentCommentCountFun,
+  comment_array = null,
+}) => {
+  let res;
+
+  await axios
+    .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog-comments", {
+      blog_id,
+      skip,
+    })
+    .then(({ data }) => {
+      data.map((comment) => {
+        comment.childrenLevel = 0;
+      });
+      setParentCommentCountFun((preVal) => !preVal + data.length);
+
+      if (comment_array == null) {
+        res = { results: data };
+      } else {
+        res = { results: [...comment_array, ...data] };
+      }
+    });
+
+  return res;
+};
+
+const CommentsContainer = () => {
+  // let {
+  //   blog: { title, comments },
+  //   commentsWrapper,
+  //   setCommentsWrapper,
+  // } = useContext(BlogContext);
+
+  // const commentsArr = comments?.results || [];
+
+  let {
+    blog: { title, comments },
+    commentsWrapper,
+    setCommentsWrapper,
+  } = useContext(BlogContext);
+
+  const commentsArr = comments?.results || [];
+
+  return (
+    <div
+      className={
+        "max-sm:w-full fixed " +
+        (commentsWrapper ? "top-0 sm:right-0" : "top-[100%] sm:right-[-100%]") +
+        " duration-700 max-sm:right-0 sm:top-0 w-[30%] min-w-[350px] h-full z-50 bg-white shadow-2xl p-8 px-16 overflow-auto overflow-x-hidden"
+      }
+    >
+      <div className="relative">
+        <div className="text-xl font-medium">Comments</div>
+        <p className="text-lg mt-2 w-[70%] to-dark-grey line-clamp-1">
+          {title}
+        </p>
+
+        <button
+          onClick={() => {
+            setCommentsWrapper((preVal) => !preVal);
+          }}
+          className="absolute top-0 right-0 flex justify-center items-center w-12 h-12 rounded-full bg-grey"
+        >
+          <i className="fi fi-br-cross text-2xl mt-1"></i>
+        </button>
+      </div>
+      <hr className="border-grey my-8 w-[120%] -ml-10" />
+
+      <CommentField action="comment" />
+
+      {commentsArr && commentsArr.length ? (
+        commentsArr.map((comment, i) => {
+          return (
+            <AnimationWrapper key={i}>
+              <CommentCard
+                index={i}
+                leftVal={comment.childrenLevel * 4}
+                commentData={comment}
+              />
+            </AnimationWrapper>
+          );
+        })
+      ) : (
+        <NoDataMessage message="No Comments" />
+      )}
+    </div>
+  );
+};
+
+export default CommentsContainer;
