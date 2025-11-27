784cb8dbb2764a8baaf705a3226cf90684a8b632
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-27 14:14:39 +0530

    add nested reply loading/hiding functionality with dynamic comment rendering

--- src/components/comment-card.component.jsx
+++ src/components/comment-card.component.jsx
@@ -3,15 +3,40 @@ import { getDay } from "../common/date";
 import { userContext } from "../App";
 import toast from "react-hot-toast";
 import CommentField from "./comment-field.component";
+import { BlogContext } from "../pages/blog.page";
+import axios from "axios";
 
 const CommentCard = ({ index, leftVal, commentData }) => {
-  let {
-    commented_by: {
-      personal_info: { profile_img, fullname, username },
-    },
-    commentedAt,
-    comment,
-  } = commentData;
+  // let {
+  //   commented_by: {
+  //     personal_info: { profile_img, fullname, username },
+  //   },
+  //   commentedAt,
+  //   comment,
+  //   _id,
+  //   children,
+  // } = commentData;
+
+  // const {
+  //   commentedAt,
+  //   comment,
+  //   _id,
+  //   children = [],
+  //   commented_by,
+  // } = commentData;
+
+  const profile = commentData?.commented_by?.personal_info || {};
+  const { profile_img, fullname, username } = profile;
+
+  const commentedAt = commentData?.commentedAt;
+  const comment = commentData?.comment;
+  const _id = commentData?._id;
+  const children = commentData?.children || [];
+
+  let { blog, setBlog } = useContext(BlogContext);
+
+  const comments = blog?.comments;
+  const commentsArr = comments?.results || [];
 
   let {
     userAuth: { access_token },
@@ -27,6 +52,56 @@ const CommentCard = ({ index, leftVal, commentData }) => {
     setReplying((preVal) => !preVal);
   };
 
+  const removeCommentsCards = (startingPoint) => {
+    if (commentsArr[startingPoint]) {
+      while (
+        commentsArr[startingPoint].childrenLevel >
+        commentsArr[index].childrenLevel
+      ) {
+        commentsArr.splice(startingPoint, 1);
+
+        if (!commentsArr[startingPoint]) {
+          break;
+        }
+      }
+    }
+
+    setBlog({ ...blog, comments: { results: commentsArr } });
+  };
+
+  const hideReplies = () => {
+    commentData.isReplyLoaded = false;
+
+    removeCommentsCards(index + 1);
+  };
+
+  const loadReplies = ({ skip = 0 }) => {
+    if (children.length) {
+      hideReplies();
+
+      axios
+        .post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies", {
+          _id,
+          skip,
+        })
+        .then(({ data: { replies } }) => {
+          commentData.isReplyLoaded = true;
+
+          for (let i = 0; i < replies.length; i++) {
+            replies[i].childrenLevel = commentData.childrenLevel + 1;
+
+            // commentsArr.splice(index + 1 + i, skip, 0, replies[i]);
+            commentsArr.splice(index + 1 + i, 0, replies[i]);
+          }
+
+          setBlog({ ...blog, comments: { ...comments, results: commentsArr } });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <div className="w-full" style={{ paddingLeft: `${leftVal * 10}px` }}>
       <div className="my-5 p-6 rounded-md border border-9">
@@ -40,13 +115,37 @@ const CommentCard = ({ index, leftVal, commentData }) => {
         <p className="font-gelasio text-xl ml-3">{comment}</p>
 
         <div className="flex gap-5 items-center mt-5">
+          {commentData.isReplyLoaded ? (
+            <button
+              className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+              onClick={hideReplies}
+            >
+              <i className="fi fi-rs-comment-dots"></i>
+              Hide Reply
+            </button>
+          ) : (
+            <button
+              className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+              onClick={loadReplies}
+            >
+              {" "}
+              <i className="fi fi-rs-comment-dots"></i>
+              {children.length} Replies
+            </button>
+          )}
+
           <button onClick={handleReplyClick} className="underline">
             Reply
           </button>
         </div>
         {isReplying ? (
           <div className="mt-8">
-            <CommentField action="reply" />
+            <CommentField
+              action="reply"
+              index={index}
+              replyingTo={_id}
+              setReplying={setReplying}
+            />
           </div>
         ) : (
           ""
