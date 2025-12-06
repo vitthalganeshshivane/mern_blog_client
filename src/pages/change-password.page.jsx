5f240a0b120b423791a4dda635e8662eb3696838
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-06 14:39:52 +0530

    add ChangePassword form with validation and JWT-protected API call

---
+++ src/pages/change-password.page.jsx
@@ -0,0 +1,94 @@
+import { useRef } from "react";
+import AnimationWrapper from "../common/page-animation";
+import InputBox from "../components/input.component";
+import { toast } from "react-hot-toast";
+import { useContext } from "react";
+import { userContext } from "../App";
+import axios from "axios";
+
+const ChangePassword = () => {
+  let {
+    userAuth: { access_token },
+  } = useContext(userContext);
+
+  let changePasswordForm = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    let form = new FormData(changePasswordForm.current);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+
+    let { currentPassword, newPassword } = formData;
+
+    if (!currentPassword.length || !newPassword.length) {
+      return toast.error("Fill all the inputs");
+    }
+
+    if (
+      !passwordRegex.test(currentPassword) ||
+      !passwordRegex.test(newPassword)
+    ) {
+      toast.error(
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters"
+      );
+    }
+
+    e.target.setAttribute("disabled", true);
+
+    let loadingToast = toast.loading("Updating......");
+
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/change-password", formData, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+      .then(() => {
+        toast.dismiss(loadingToast);
+        e.target.removeAttribute("disabled");
+        return toast.success("Password Updated");
+      })
+      .catch(({ response }) => {
+        toast.dismiss(loadingToast);
+        e.target.removeAttribute("disabled");
+        return toast.error(response.data.error);
+      });
+  };
+
+  return (
+    <AnimationWrapper>
+      <form ref={changePasswordForm}>
+        <h1 className="max-md:hidden">Change Password</h1>
+
+        <div className="py-10 w-full md:max-w-[400px]">
+          <InputBox
+            name="currentPassword"
+            type="password"
+            className="profile-edit-input"
+            placeholder="Current Password"
+            icon="fi-rr-unlock"
+          />
+          <InputBox
+            name="newPassword"
+            type="password"
+            className="profile-edit-input"
+            placeholder="New Password"
+            icon="fi-rr-lock"
+          />
+          <button onClick={handleSubmit} className="btn-dark px-10">
+            Change Password
+          </button>
+        </div>
+      </form>
+    </AnimationWrapper>
+  );
+};
+
+export default ChangePassword;
