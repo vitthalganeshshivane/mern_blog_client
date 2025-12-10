c8052758283b36525bb21463109d3f5105011814
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-11 01:37:11 +0530

    add Notifications page with basic type filters (all/like/comment/reply)

---
+++ src/pages/notifications.page.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Notifications = () => {
+  const [filter, setFilter] = useState("all");
+
+  let filters = ["all", "like", "comment", "reply"];
+
+  const handleFilter = (e) => {
+    let btn = e.target;
+
+    setFilter(btn.innerHTML);
+  };
+
+  return (
+    <div>
+      <h1 className="max-md:hidden">Recent Notifications</h1>
+
+      <div className="my-8 flex gap-6">
+        {filters.map((filterName, i) => {
+          return (
+            <button
+              key={i}
+              className={
+                "py-2 " + (filter == filterName ? "btn-dark" : "btn-light")
+              }
+              onClick={handleFilter}
+            >
+              {filterName}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
