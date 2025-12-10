6f431b63f89141d8c15097c5101a46852cc8da29
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-11 02:32:17 +0530

    enhance LoadMoreDataBtn to support additional params and safer state access

--- src/components/load-more.component.jsx
+++ src/components/load-more.component.jsx
@@ -1,8 +1,10 @@
-const LoadMoreDataBtn = ({ state, fetchDataFun }) => {
-  if (state !== null && state.totalDocs > state.results.length) {
+const LoadMoreDataBtn = ({ state, fetchDataFun, additionalParam }) => {
+  if (state !== null && state.totalDocs > state?.results?.length) {
     return (
       <button
-        onClick={() => fetchDataFun({ page: state.page + 1 })}
+        onClick={() =>
+          fetchDataFun({ ...additionalParam, page: state.page + 1 })
+        }
         className="text-dark-grey p-2 hover:bg-grey/30 rounded-md flex items-center gap-2"
       >
         Load More
