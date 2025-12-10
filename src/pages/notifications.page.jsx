1467b5198e971e9300bf8719a55cea3773ae02c2
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-11 02:10:09 +0530

    add fetch filtered notifications with pagination using protected API

--- src/pages/notifications.page.jsx
+++ src/pages/notifications.page.jsx
@@ -1,9 +1,51 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { userContext } from "../App";
+import { filterPaginationData } from "../common/filter-pagination-data";
 
 const Notifications = () => {
+  let {
+    userAuth: { access_token },
+  } = useContext(userContext);
+
   const [filter, setFilter] = useState("all");
+  const [notifications, setNotifications] = useState(null);
 
   let filters = ["all", "like", "comment", "reply"];
+
+  const fetchNotifications = ({ page, deletedDocCount = 0 }) => {
+    axios
+      .post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/notifications",
+        { page, filter, deletedDocCount },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      .then(async ({ data: { Notifications: data } }) => {
+        let formatedData = await filterPaginationData({
+          state: notifications,
+          data,
+          page,
+          countRoute: "/all-notifications-count",
+          data_to_send: { filter },
+          user: access_token,
+        });
+
+        setNotifications(formatedData);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    if (access_token) {
+      fetchNotifications({ page: 1 });
+    }
+  }, [access_token, filter]);
 
   const handleFilter = (e) => {
     let btn = e.target;
