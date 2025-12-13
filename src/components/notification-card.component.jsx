39489ad1cb5941d16e8d973d7782013111867c9e
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-13 17:48:41 +0530

    add delete comment/reply and reply preview to NotificationCard

--- src/components/notification-card.component.jsx
+++ src/components/notification-card.component.jsx
@@ -3,12 +3,15 @@ import { getDay } from "../common/date";
 import { useContext, useState } from "react";
 import NotificationCommentField from "./notification-comment-field.component";
 import { userContext } from "../App";
+import axios from "axios";
 
 const NotificationCard = ({ data, index, notificationState }) => {
   let [isReplying, setReplying] = useState(false);
 
   let {
+    seen,
     type,
+    reply,
     createdAt,
     comment,
     replied_on_comment,
@@ -20,12 +23,62 @@ const NotificationCard = ({ data, index, notificationState }) => {
     _id: notification_id,
   } = data;
 
+  let {
+    userAuth: {
+      username: author_username,
+      profile_img: author_profile_img,
+      access_token,
+    },
+  } = useContext(userContext);
+
+  let {
+    notifications,
+    notifications: { results, totalDocs },
+    setNotifications,
+  } = notificationState;
+
   const handleReplyClick = () => {
     setReplying((preVal) => !preVal);
   };
 
+  const handleDelete = (comment_id, type, target) => {
+    target.setAttribute("disabled", true);
+    axios
+      .post(
+        import.meta.env.VITE_SERVER_DOMAIN + "/delete-comment",
+        { _id: comment_id },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      )
+      .then(() => {
+        if (type == "comment") {
+          results.splice(index, 1);
+        } else {
+          delete results[index].reply;
+        }
+
+        target.removeAttribute("disabled");
+        setNotifications({
+          ...notifications,
+          results,
+          totalDocs: totalDocs - 1,
+          deletedDocCount: notifications.deletedDocCount - 1,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="p-6 border-b border-grey border-l-black">
+    <div
+      className={
+        "p-6 border-b border-grey border-l-black " + (!seen ? "border-l-2" : "")
+      }
+    >
       <div className="flex gap-5 mb-3">
         <img
           src={profile_img}
@@ -75,13 +128,22 @@ const NotificationCard = ({ data, index, notificationState }) => {
         <p>{getDay(createdAt)}</p>
         {type !== "like" ? (
           <>
+            {!reply ? (
+              <button
+                onClick={handleReplyClick}
+                className="underline hover:text-black"
+              >
+                Reply
+              </button>
+            ) : (
+              ""
+            )}
             <button
-              onClick={handleReplyClick}
+              onClick={(e) => handleDelete(comment._id, "comment", e.target)}
               className="underline hover:text-black"
             >
-              Reply
+              Delete
             </button>
-            <button className="underline hover:text-black">Delete</button>
           </>
         ) : (
           ""
@@ -99,6 +161,44 @@ const NotificationCard = ({ data, index, notificationState }) => {
             notification_id={notification_id}
             notificationData={notificationState}
           />
+        </div>
+      ) : (
+        ""
+      )}
+
+      {reply ? (
+        <div className="ml-20 p-5 bg-grey mt-5 rounded-md ">
+          <div className="flxe gap-3 mb-3">
+            <img
+              src={author_profile_img}
+              alt=""
+              className="w-8 h-8 rounded-full"
+            />
+            <div className="">
+              <h1 className="font-medium text-xl text-dark-grey">
+                <Link
+                  className="mx-1 text-black underline"
+                  to={`/user/${author_username}`}
+                >
+                  @{author_username}
+                </Link>
+                <span className="font-normal">replied to</span>
+                <Link
+                  className="mx-1 text-black underline"
+                  to={`/user/${username}`}
+                >
+                  @{username}
+                </Link>
+              </h1>
+            </div>
+          </div>
+          <p className="ml-14 font-gelasio text-xl my-2">{reply.comment}</p>
+          <button
+            onClick={(e) => handleDelete(comment._id, "reply", e.target)}
+            className="underline hover:text-black ml-14 mt-2"
+          >
+            Delete
+          </button>
         </div>
       ) : (
         ""
