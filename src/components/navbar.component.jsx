8b3c3a29329318002daf583d19264353b5deb790
vitthal4545 <vitthalgs1434@gmail.com>
2025-12-13 17:47:39 +0530

    add dark mode toggle, theme-aware logo, and notification badge in Navbar

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,12 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, Navigate, Outlet, useNavigate } from "react-router-dom";
-import logo from "../imgs/logo.png";
-import { userContext } from "../App";
+import darkLogo from "../imgs/logo-dark.png";
+import lightLogo from "../imgs/logo-light.png";
+import { ThemeContext, userContext } from "../App";
 import UserNavigationPanel from "./user-navigation.component";
+import axios from "axios";
+import { storeInSession } from "../common/session";
 
 function Navbar() {
   const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
   const [userNavPanel, setUserNavPanel] = useState(false);
+
+  let { theme, setTheme } = useContext(ThemeContext);
 
   let navigate = useNavigate();
 
@@ -15,8 +20,30 @@ function Navbar() {
   //   userAuth: { access_token, profile_img },
   // } = useContext(userContext);
 
-  const { userAuth = {} } = useContext(userContext) || {};
-  const { access_token, profile_img } = userAuth;
+  const { userAuth = {}, setUserAuth } = useContext(userContext) || {};
+  const { access_token, profile_img, new_notification_available, isAdmin } =
+    userAuth;
+
+  useEffect(() => {
+    if (access_token) {
+      axios
+        .post(
+          import.meta.env.VITE_SERVER_DOMAIN + "/new-notification",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        )
+        .then(({ data }) => {
+          setUserAuth({ ...userAuth, ...data });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [access_token]);
 
   const handleUserNavPanel = () => {
     setUserNavPanel((currentVal) => !currentVal);
@@ -37,11 +64,25 @@ function Navbar() {
     }
   };
 
+  const changeTheme = () => {
+    let newTheme = theme === "light" ? "dark" : "light";
+
+    setTheme(newTheme);
+
+    document.body.setAttribute("data-theme", newTheme);
+
+    storeInSession("theme", newTheme);
+  };
+
   return (
     <>
       <nav className="navbar z-50">
         <Link to="/" className="flex-none w-10">
-          <img src={logo} alt="" className="w-full" />
+          <img
+            src={theme == "light" ? darkLogo : lightLogo}
+            alt=""
+            className="w-full"
+          />
         </Link>
         <div
           className={
@@ -65,16 +106,38 @@ function Navbar() {
             <i className="fi fi-rr-search text-xl"></i>
           </button>
 
-          <Link to="/editor" className="hidden md:flex gap-2 link">
-            <i className="fi fi-rr-file-edit text-xl"></i>
-            <p>Write</p>
-          </Link>
+          {isAdmin ? (
+            <Link to="/editor" className="hidden md:flex gap-2 link">
+              <i className="fi fi-rr-file-edit text-xl"></i>
+              <p>Write</p>
+            </Link>
+          ) : (
+            ""
+          )}
+
+          <button
+            className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10"
+            onClick={changeTheme}
+          >
+            <i
+              className={
+                "fi fi-rr-" +
+                (theme == "light" ? "moon-stars" : "sun") +
+                " text-xl"
+              }
+            ></i>
+          </button>
 
           {access_token ? (
             <>
-              <Link to="/dashboard-notification">
+              <Link to="/dashboard/notifications">
                 <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                   <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+                  {new_notification_available ? (
+                    <span className="bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2"></span>
+                  ) : (
+                    ""
+                  )}
                 </button>
               </Link>
 
